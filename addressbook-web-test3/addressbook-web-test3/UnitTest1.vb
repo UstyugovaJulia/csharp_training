9cd3964ecf55288b9b0eb5071c7a6d00de762aa2
bublikjulija@gmail.com <bublikjulija@gmail.com>
2021-10-14 15:01:16 +0600

    Проект создан

---
+++ addressbook-web-test3/addressbook-web-test3/UnitTest1.vb
@@ -0,0 +1,18 @@
+Imports NUnit.Framework
+
+Namespace addressbook_web_test3
+
+    Public Class Tests
+
+        <SetUp>
+        Public Sub Setup()
+        End Sub
+
+        <Test>
+        Public Sub Test1()
+            Assert.Pass()
+        End Sub
+
+    End Class
+
+End Namespace
