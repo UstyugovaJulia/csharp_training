8b7a51d129c0258c7d0e0b0fcd6331cc28dd8ae3
bublikjulija@gmail.com <bublikjulija@gmail.com>
2021-10-18 18:29:11 +0600

    get set

---
+++ addresbook-web-tests2/addresbook-web-tests2/Square.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addresbook_web_tests2
+{
+    class Square
+    {
+        private int size;
+
+        public Square(int size)
+        {
+            this.size = size;
+        }
+        public int getSize()
+        {
+            return size;
+        }
+
+        public void setSize(int size)
+        {
+            this.size = size;
+
+        }
+    }
+
+}
---
+++ addresbook-web-tests2/addresbook-web-tests2/UnitTest2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace addresbook_web_tests2
+{
+    [TestClass]
+    public class UnitTest2
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.getSize(), 5);
+            Assert.AreEqual(s2.getSize(), 10);
+            Assert.AreEqual(s3.getSize(), 5);
+
+            s3.setSize(15);
+
+            Assert.AreEqual(s1.getSize(), 15);
+        }
+    }
+}
