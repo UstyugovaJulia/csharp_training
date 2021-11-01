ededb2cbc80bcb90a34a47bbf5bb90180b0622b2
bublikjulija@gmail.com <bublikjulija@gmail.com>
2021-11-01 16:15:47 +0600

    Лекция 3_3

--- addresbook-web-tests2/addresbook-web-tests2/tests/ContactEditTests.cs
+++ addresbook-web-tests2/addresbook-web-tests2/tests/ContactEditTests.cs
@@ -10,7 +10,7 @@ using OpenQA.Selenium.Support.UI;
 namespace WebAddressbookTests
 {
     [TestFixture]
-    public class ContactEditTests : TestBase
+    public class ContactEditTests : AuthTestBase
     {
 
 
